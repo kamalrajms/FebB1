831176675869375473974845e3739eda8dc1e52c
kamalrajms <kamalrajindtn@gmail.com>
2026-02-23 11:46:14 +0530

    FebB1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Greeting from "./Greeting";
+
+// import
+
+export default function App() {
+  // functionality
+  return (
+    // structure
+    <div>
+      <h1>Hello everyone</h1>
+      <Greeting />
+    </div>
+  );
+}
---
+++ src/Greeting.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Good from './Good'
+
+export default function Greeting() {
+  return (
+    <div>
+      <h2>React.js class</h2>
+      <Good/>
+    </div>
+  )
+}
